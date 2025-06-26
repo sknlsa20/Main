de17dd3e815e20e7c4abad6d47ba975fd284f06b
sknlsa20 <arutla.shivakumar@gmail.com>
2025-06-27 01:17:30 +0530

    general

---
+++ webscrapping/src/main/java/com/example/mn/WebscrappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebscrappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebscrappingApplication.class, args);
+	}
+
+}
